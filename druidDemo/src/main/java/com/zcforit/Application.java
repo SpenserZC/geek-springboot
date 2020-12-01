cccd358949f0e5a5ca90871bd68b1e40eff72d6d
zczczc <spenserzc@outlook.com>
2020-12-01 20:53:36 +0800

    20201201

---
+++ druidDemo/src/main/java/com/zcforit/Application.java
@@ -0,0 +1,30 @@
+package com.zcforit;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@SpringBootApplication
+@Slf4j
+@EnableTransactionManagement(proxyTargetClass = true)
+public class Application implements CommandLineRunner {
+    @Autowired
+    private DataSource dataSource;
+    @Autowired
+    private FooService fooService;
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+    @Override
+    public void run(String... args) throws Exception {
+        log.info(dataSource.toString());
+        new Thread(() -> fooService.selectForUpdate()).start();
+        new Thread(() -> fooService.selectForUpdate()).start();
+    }
+}
